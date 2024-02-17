10c6a42dab9c0a15aaa33c0d3c14025226bc6b21
Jose David Lagos Morales <especialista_nsrtm19@mef.gob.pe>
2024-02-17 12:21:02 -0500

    subida final

--- application/src/main/java/com/codigo/msregistro/application/controller/PersonaController.java
+++ application/src/main/java/com/codigo/msregistro/application/controller/PersonaController.java
@@ -3,6 +3,8 @@ package com.codigo.msregistro.application.controller;
 import com.codigo.msregistro.domain.aggregates.dto.PersonaDTO;
 import com.codigo.msregistro.domain.aggregates.request.RequestPersona;
 import com.codigo.msregistro.domain.ports.in.PersonaServiceIn;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,10 +15,12 @@ import java.util.List;
 @RestController
 @RequestMapping("/v2/persona")
 @RequiredArgsConstructor
+@OpenAPIDefinition
 public class PersonaController {
 
     private final PersonaServiceIn personaServiceIn;
 
+    @Operation(summary = "crear persona")
     @PostMapping("/crear")
     public ResponseEntity<PersonaDTO> registrar(@RequestBody RequestPersona requestPersona){
         return ResponseEntity
@@ -24,6 +28,7 @@ public class PersonaController {
                 .body(personaServiceIn.crearPersonaIn(requestPersona));
     }
 
+    @Operation(summary = "obtener persona por Id")
     @GetMapping("/{id}")
     public ResponseEntity<PersonaDTO>obtenerPersona(@PathVariable Long id){
         return ResponseEntity
@@ -31,6 +36,8 @@ public class PersonaController {
                 .body(personaServiceIn.obtenerPersonaIn(id).get());
 
     }
+
+    @Operation(summary = "listar personas")
     @GetMapping("/listar")
     public ResponseEntity<List<PersonaDTO>>obtenerTodos(){
 
@@ -39,6 +46,8 @@ public class PersonaController {
                 .body(personaServiceIn.obtenerTodosIn());
 
     }
+
+    @Operation(summary = "actualizar personas por Id")
     @PutMapping("/actualizar/{id}")
     public ResponseEntity<PersonaDTO>actualizar(@PathVariable Long id,@RequestBody RequestPersona requestPersona){
         return ResponseEntity
@@ -47,6 +56,7 @@ public class PersonaController {
 
     }
 
+    @Operation(summary = "eliminar personas por Id")
     @DeleteMapping("/eliminar/{id}")
     public ResponseEntity<PersonaDTO>eliminar(@PathVariable Long id){
         return ResponseEntity
